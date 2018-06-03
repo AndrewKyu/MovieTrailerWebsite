9b0031e691519595d1c333b75b4d4430897c4401
AndrewKyu <andrewkyu95@gmail.com>
2018-06-02 18:08:43 -0700

    enhanced media class, integrated webbrowser Python library

---
+++ entertainment_center.py
@@ -0,0 +1,19 @@
+import media
+
+toy_story = media.Movie("Toy Story", 
+	"A story of a boy and his toys that come to life",
+	"https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+	 "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+	"A marine on an alien planet",
+	"https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+pokemon_the_first_movie = media.Movie("Pokemon the First Movie",
+	"A story about how Ash Ketchum meets Mewtwo",
+	"https://vignette.wikia.nocookie.net/pokemon/images/5/57/MS001.png/revision/latest?cb=20110215105244",
+	"https://www.youtube.com/watch?v=sSLuNZFa_3k")
+
+#print(avatar.storyline)
+pokemon_the_first_movie.show_trailer()
